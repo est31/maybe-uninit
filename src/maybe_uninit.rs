1d638fc155593025e7db27b8da93a3b2948cb51d
Matias Rodriguez <rodrimatt1985@gmail.com>
2019-07-09 03:53:55 -0300

    Modify tests to work in no_std

--- src/maybe_uninit.rs
+++ src/maybe_uninit.rs
@@ -605,20 +605,45 @@ impl<T> MaybeUninit<T> {
 }
 
 
-
 #[cfg(test)]
 mod tests{
     use super::*;
 
+    #[cfg(no_std)]
+    use core::cell::Cell;
+    #[cfg(not(no_std))]
+    use std::cell::Cell;   
+
+    struct DecrementOnDrop<'a>(&'a Cell<usize>);
+
+    impl<'a> DecrementOnDrop<'a>{
+        pub fn new(ref_:&'a Cell<usize>)->Self{
+            ref_.set(1);
+            DecrementOnDrop(ref_)
+        }
+    }
+
+    impl<'a> Clone for DecrementOnDrop<'a>{
+        fn clone(&self)->Self{
+            self.0.set(self.0.get()+1);
+
+            DecrementOnDrop(self.0)
+        }
+    }
+
+    impl<'a> Drop for DecrementOnDrop<'a>{
+        fn drop(&mut self){
+            self.0.set(self.0.get()-1);
+        }
+    }
+
     #[test]
     fn doesnt_drop(){
-        unsafe{
-            use std::sync::Arc;
-            let arc=Arc::new(0);
-            let maybe=MaybeUninit::new(arc.clone());
-            assert_eq!(Arc::strong_count(&*maybe.as_ptr()), 2);
-            drop(maybe);
-            assert_eq!(Arc::strong_count(&arc), 2);
-        }
+        let count=Cell::new(0);
+        let arc=DecrementOnDrop::new(&count);
+        let maybe=MaybeUninit::new(arc.clone());
+        assert_eq!(count.get(), 2);
+        drop(maybe);
+        assert_eq!(count.get(), 2);
     }
 }
